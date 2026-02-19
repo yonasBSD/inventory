c051a3593df7b88f571747e8006e6c493da22ce4
David Tolnay <dtolnay@gmail.com>
2026-02-19 07:50:13 -0800

    Release 0.3.22

--- src/lib.rs
+++ src/lib.rs
@@ -147,7 +147,7 @@
 //! }
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/inventory/0.3.21")]
+#![doc(html_root_url = "https://docs.rs/inventory/0.3.22")]
 #![no_std]
 #![deny(unsafe_op_in_unsafe_fn)]
 #![allow(
